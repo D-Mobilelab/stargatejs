9dd27d0247606f9121ae691dd9ea04e2ca12597c
pasquale <pasqualemangialavori@buongiorno.com>
2016-07-05 12:59:31 +0200

    unzip mock

--- test/helpers/cordova-plugin-unzip.js
+++ test/helpers/cordova-plugin-unzip.js
@@ -1,41 +1,63 @@
-function UnzipMock(){
+/**
+ * @class
+ */
+function UnzipOperationClass(fileUrl, dirUrl, cbs, progress){
+    var self = this;
     this.progressIntervalID = null;
     this.progressEvent = { loaded: 0, total: 100 };
+    this.onprogress = progress;
+    this.onsuccess = cbs;
+    this.whenFail = -1;
+    self.progressIntervalID = setInterval(() => {
+        if (self.progressEvent.loaded === 100){
+            cbs(0);
+            clearInterval(self.progressIntervalID);
+            self.progressEvent = { loaded: 0, total: 100 };
+        } else if (self.whenFail && self.whenFail === self.progressEvent.loaded) {
+            cbs(1);
+            clearInterval(self.progressIntervalID);
+            self.progressEvent = { loaded: 0, total: 100 };
+        } else {
+            progress(self.progressEvent);
+            self.progressEvent.loaded += 1;
+        }
+    }, 10);
 }
 
-UnzipMock.prototype.failAt = function(at){
-    this.whenFail = at;
+UnzipOperationClass.prototype.fail = function(){
+    clearInterval(this.progressIntervalID);
+    this.onsuccess(1);    
+    this.progressEvent = { loaded: 0, total: 100 };
 };
+
+/**
+ * @class
+ */
+function UnzipMock(){
+    this.operations = [];
+}
 
 UnzipMock.prototype.install = function(){
     var self = this;
     if (!window.zip){
         window.zip = {};
         window.zip.unzip = function(fileUrl, dirUrl, cbs, progress){
-
-            self.progressIntervalID = setInterval(function(){
-                if (self.progressEvent.loaded === 100){
-                    cbs(0);
-                    clearInterval(self.progressIntervalID);
-                    self.progressEvent = { loaded: 0, total: 100 };
-                } else if (self.whenFail && self.whenFail === self.progressEvent.loaded) {
-                    cbs(1);
-                    clearInterval(self.progressIntervalID);
-                    self.progressEvent = { loaded: 0, total: 100 };
-                } else {
-                    progress(self.progressEvent);
-                    self.progressEvent.loaded += 1;
-                }
-            }, 10);
-        }
+            var operation = new UnzipOperationClass(fileUrl, dirUrl, cbs, progress);
+            self.operations.push(operation);            
+        };
     }
 };
 
 UnzipMock.prototype.uninstall = function(){
     if (window.zip){
-        clearInterval(this.progressIntervalID);
         delete window.zip;
+        this.operations = [];
     }
 };
 
-export {UnzipMock};
+UnzipMock.prototype.getLastOperation = function(){
+    return this.operations[this.operations.length - 1];
+};
+
+var unzipMock = new UnzipMock();
+export default unzipMock;
